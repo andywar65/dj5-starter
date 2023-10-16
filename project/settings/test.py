10e2fd2c48c8c13e8f8dd9be9f016a4d8e11fc53
andywar65 <andy.war1965@gmail.com>
2023-10-16 21:57:00 +0200

    added socialapp in test settings

--- project/settings/test.py
+++ project/settings/test.py
@@ -1,3 +1,16 @@
 from .base import *  # noqa
 
 DEBUG = False
+
+SOCIALACCOUNT_PROVIDERS = {
+    "google": {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        "APP": {
+            "client_id": env.str("SOCIALAPP_CLIENT_ID"),  # noqa
+            "secret": env.str("SOCIALAPP_SECRET"),  # noqa
+            "key": "",
+        },
+    }
+}
