20c56409896bc5b698db99319cae50c3ee288b52
andywar65 <andy.war1965@gmail.com>
2023-10-17 19:25:29 +0200

    date dynamic footer

--- project/processors.py
+++ project/processors.py
@@ -1,7 +1,14 @@
+from django.utils.timezone import now
+
 from users.models import Logo
 
 
 def get_navbar_footer_data(request):
     logo = Logo.objects.first()
     links = "Bar"
-    return {"logo": logo, "f_links": links}
+    this_year = now().date().year
+    return {
+        "logo": logo,
+        "f_links": links,
+        "this_year": this_year,
+    }
