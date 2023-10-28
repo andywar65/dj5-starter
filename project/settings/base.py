caa09d562f6dbd8948679327dead89e629f53df0
andywar65 <andy.war1965@gmail.com>
2023-10-28 19:53:24 +0200

    deleted bootstrap from settings

--- project/settings/base.py
+++ project/settings/base.py
@@ -60,7 +60,6 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
-    "bootstrap5",
     "crispy_forms",
     "crispy_bootstrap5",
     "django_htmx",
