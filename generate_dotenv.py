22310a3ad110d1def60166d3e1d4537aeca30e4e
andywar65 <andy.war1965@gmail.com>
2023-10-30 18:02:44 +0100

    adding new lines

--- generate_dotenv.py
+++ generate_dotenv.py
@@ -5,12 +5,12 @@ from django.core.management.utils import get_random_secret_key
 DIR = Path(__file__).resolve().parent
 
 with open(Path(DIR / ".env"), "w") as f:
-    f.write("DEBUG=True")
-    f.write("SECRET_KEY=" + get_random_secret_key())
-    f.write("DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME")
-    f.write("EMAIL_BACKEND=")
-    f.write("STATIC_ROOT=/path/to/your/static_app")
-    f.write("STATIC_URL=/static/")
-    f.write("MEDIA_ROOT=/path/to/your/static_app")
+    f.write("DEBUG=True\n")
+    f.write("SECRET_KEY=" + get_random_secret_key() + "\n")
+    f.write("DATABASE_URL=\n")
+    f.write("EMAIL_BACKEND=\n")
+    f.write("STATIC_ROOT=/path/to/your/static_app\n")
+    f.write("STATIC_URL=/static/\n")
+    f.write("MEDIA_ROOT=/path/to/your/static_app\n")
     f.write("MEDIA_URL=/media/")
 f.close()
