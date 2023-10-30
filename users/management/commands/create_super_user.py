03f73d6d1f69fbcc0dcdfbb979fd64c77212a126
andywar65 <andy.war1965@gmail.com>
2023-10-30 18:40:20 +0100

    Instructions for further steps

--- users/management/commands/create_super_user.py
+++ users/management/commands/create_super_user.py
@@ -14,7 +14,16 @@ class Command(BaseCommand):
 
         create_item()
 
-        self.stdout.write("Done.")
+        self.stdout.write("Done, you can log in as 'andywar65', pw='P4s5W0r6'.")
+        self.stdout.write(
+            "Remember that this is a dummy installation, it works on a Sqlite database."
+        )
+        self.stdout.write(
+            "Create a Postgres database, update dotenv file (db.url and media/static roots)"
+        )
+        self.stdout.write(
+            "then migrate and create a super user again."
+        )
 
 
 def create_item():
