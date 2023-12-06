d7997ed29042a5321f89c1eeeced0a61e29dc867
andywar65 <andy.war1965@gmail.com>
2023-12-06 13:25:18 +0100

    commented database url

--- users/management/commands/generate_dot_env.py
+++ users/management/commands/generate_dot_env.py
@@ -31,7 +31,8 @@ def create_item():
     with open(Path(settings.BASE_DIR / ".env"), "w") as f:
         f.write("DEBUG=True\n")
         f.write("SECRET_KEY=" + get_random_secret_key() + "\n")
-        f.write("DATABASE_URL=sqlite://dj5-starter/db.sqlite3\n")
+        f.write("# change database url and uncomment it\n")
+        f.write("# DATABASE_URL=postgres://postgres:postgres@db:5432/postgres\n")
         f.write("EMAIL_BACKEND=django.core.mail.backends.console.EmailBackend\n")
         f.write("STATIC_ROOT=/path/to/your/static_app\n")
         f.write("STATIC_URL=/static/\n")
