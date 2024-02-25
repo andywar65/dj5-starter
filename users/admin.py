6b473cf4f6c792ba1df17e7a421231f4a2c79097
andywar65 <andy.war1965@gmail.com>
2024-02-25 15:25:09 +0100

    excluding avatar in admin

--- users/admin.py
+++ users/admin.py
@@ -41,7 +41,7 @@ admin.site.register(FlatPage, TinyMCEFlatPageAdmin)
 
 class ProfileAdmin(TranslationTabularInline):
     model = Profile
-    # exclude = ("temp_image",)
+    exclude = ("avatar",)
     extra = 0
 
 
