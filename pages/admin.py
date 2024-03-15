c58e312c3921397ec90cf48abc2c3e73f6d549e8
andywar65 <andy.war1965@gmail.com>
2024-03-15 22:19:46 +0100

    added shotgun models

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
 
-# Register your models here.
+from .models import Shotgun, ShotgunImage
+
+
+class ShotgunImageInline(admin.TabularInline):
+    model = ShotgunImage
+    fields = (
+        "position",
+        "description",
+        "filer_image",
+    )
+    extra = 0
+
+
+@admin.register(Shotgun)
+class ShotgunAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "date",
+    )
+    exclude = ("image",)
+    inlines = [
+        ShotgunImageInline,
+    ]
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,56 @@
-from django.db import models  # noqa
+from django.db import models
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
+from filer.fields.image import FilerImageField
+from tinymce.models import HTMLField
 
-# Create your models here.
+
+class Shotgun(models.Model):
+    title = models.CharField(
+        _("Title"), help_text=_("The title of the article"), max_length=100
+    )
+    body = HTMLField(_("Text"), null=True)
+    date = models.DateField(
+        _("Date"),
+        default=now,
+    )
+
+    class Meta:
+        verbose_name = _("Shotgun article")
+        verbose_name_plural = _("Shotgun articles")
+        ordering = [
+            "-date",
+        ]
+
+    def get_card_width(self):
+        for img in self.shotgun_image.all():
+            if img.filer_image.width > img.filer_image.height:
+                return "max-width: 960px"
+        return "max-width: 480px"
+
+
+class ShotgunImage(models.Model):
+    shot = models.ForeignKey(
+        Shotgun,
+        on_delete=models.CASCADE,
+        related_name="shotgun_image",
+        verbose_name=_("Article"),
+    )
+    description = models.CharField(
+        _("Description"),
+        help_text=_("Used in captions"),
+        max_length=200,
+        null=True,
+        blank=True,
+    )
+    filer_image = FilerImageField(
+        null=True, related_name="shotgun_filer_image", on_delete=models.SET_NULL
+    )
+    position = models.PositiveSmallIntegerField(_("Position"), null=True)
+
+    class Meta:
+        verbose_name = _("Shotgun image")
+        verbose_name_plural = _("Shotgun images")
+        ordering = [
+            "position",
+        ]
