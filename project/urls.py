30ae420d181b07211ceb1929632d601c35611ccc
andywar65 <andy.war1965@gmail.com>
2024-03-15 23:39:34 +0100

    added templates

--- project/urls.py
+++ project/urls.py
@@ -40,7 +40,7 @@ from users.views import (
     profile_update_delete,
 )
 
-from .views import home, nav_bar, search_box, search_results
+from .views import nav_bar, search_box, search_results
 
 # from django.views.generic import RedirectView
 
@@ -88,8 +88,7 @@ urlpatterns = [
 
 urlpatterns += i18n_patterns(
     path(_("search/"), search_results, name="search_results"),
-    path("", home, name="home"),
-    path("", ShotgunArchiveIndexView.as_view(), name="shotgun_index"),
+    path("", ShotgunArchiveIndexView.as_view(), name="home"),
 )
 
 urlpatterns += [
