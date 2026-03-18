c4fc6e345b5ba3643a168e076e6c7dbb1895e7b3
andywar65 <andy.war1965@gmail.com>
2026-03-18 14:14:48 +0100

    added legacy shotgun detail urlpattern

--- pages/urls.py
+++ pages/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
         name="shotgun_detail",
     ),
     path(
+        "shot/<int:pk>/",
+        ShotgunArchiveLimited.as_view(),
+        name="shotgun_detail_legacy",
+    ),
+    path(
         "add/",
         ShotgunCreateFormView.as_view(),
         name="shotgun_create",
